2d6cc65ae3d6fec617e8550071e8ce5822fae379
Luis Perez <perezluis167@gmail.com>
2023-08-06 02:57:46 -0400

    Solved remove_element.js

--- remove_element.js
+++ remove_element.js
@@ -1,29 +1,36 @@
 /**
- * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+ * Given an integer array nums and an integer val, 
+ * remove all occurrences of val in nums in-place.
+ * The order of the elements may be changed. 
+ * Then return the number of elements in nums which are not equal to val.
 
-Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+Consider the number of elements in nums which are not equal to val be k, 
+    to get accepted, 
+    you need to do the following things:
 
-Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
-Return k.
+Change the array nums such that the first k elements of nums contain
+    the elements which are not equal to val. 
+    The remaining elements of nums are not important as well as the size of nums.
+    Return k.
+
 Custom Judge:
 
-The judge will test your solution with the following code:
+    The judge will test your solution with the following code:
 
-int[] nums = [...]; // Input array
-int val = ...; // Value to remove
-int[] expectedNums = [...]; // The expected answer with correct length.
+    int[] nums = [...]; // Input array
+    int val = ...; // Value to remove
+    int[] expectedNums = [...]; // The expected answer with correct length.
                             // It is sorted with no values equaling val.
 
-int k = removeElement(nums, val); // Calls your implementation
+    int k = removeElement(nums, val); // Calls your implementation
 
-assert k == expectedNums.length;
-sort(nums, 0, k); // Sort the first k elements of nums
-for (int i = 0; i < actualLength; i++) {
-    assert nums[i] == expectedNums[i];
-}
+    assert k == expectedNums.length;
+    sort(nums, 0, k); // Sort the first k elements of nums
+        for (int i = 0; i < actualLength; i++) {
+            assert nums[i] == expectedNums[i];
+        }
+
 If all assertions pass, then your solution will be accepted.
-
- 
 
 Example 1:
 
@@ -46,3 +53,33 @@ Constraints:
 0 <= nums[i] <= 50
 0 <= val <= 100
  */
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function (nums, val) {
+
+    let counter = 0;
+    for (let i = 0; i < nums.length; i++) {
+        const element = nums[i];
+        if (element === val) {
+            // nums.push(element)
+            nums.splice(i, 1);
+            nums.unshift(val);
+            counter += 1;
+            continue;
+        }
+    }
+    for (let i = 0; i < counter; i++) {
+        nums.shift();
+    }
+
+    return nums.length;
+
+};
+
+const nums = [0,1,2,3], val = 5;
+console.log(removeElement(nums, val));
+console.log(nums);

00ef2fb717b25185b044327894dc44efc871ace1
Luis Perez <perezluis167@gmail.com>
2023-08-29 22:06:59 -0400

    Not correct but in the right track

--- best_time_to_buy_and_sell_stock.js
+++ best_time_to_buy_and_sell_stock.js
@@ -1,11 +1,12 @@
 /**
  * You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+You want to maximize your profit by choosing a single day
+to buy one stock and choosing a different day in the future
+to sell that stock.
 
-Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-
- 
+Return the maximum profit you can achieve from this transaction.
+If you cannot achieve any profit, return 0.
 
 Example 1:
 
@@ -30,6 +31,36 @@ Constraints:
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = function(prices) {
+const maxProfit = function (prices) {
+    
+    if (prices.length === 0) return 0;
+    
+    let currentMax = prices[0];
+    let currentMin = prices[0];
+    let maxDay = 0;
+    let minDay = 0;
+    
+    for (let i = 0; i < prices.length; i++) {
+        const price = prices[i];
+        
+        if (price > currentMax) {
+            currentMax = price;
+            maxDay = i;
+        }
+        
+        if (price < currentMin) {
+            currentMin = price;
+            minDay = i;
+        }
+        
+        if (currentMax === 104 && currentMin === 0) {
+            break;
+        }
+    }
+
+    return prices[maxDay] - prices[minDay];
     
 };
+
+const prices = [7, 1, 5, 3, 6, 4]; // => 5
+console.log(maxProfit(prices));
